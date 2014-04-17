fc04259dbf361ccca88c4efce1fb5e26540e2e58
Bruce McLeod <bruce@Veyron.local>
2014-04-17 19:15:44 +1000

    Added in mono port of stax framework

---
+++ mono/Parkcalc/Parkcalc.SeleniumTests/Test.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Parkcalc.SeleniumTests
+{
+	[TestFixture ()]
+	public class Test : BaseTest
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+            string lblMessage = "id='message'";
+
+
+
+			Browser.NavigateTo ("http://127.0.0.1:8080");
+
+
+
+
+            string text = Browser.GetValue(lblMessage);
+
+
+
+
+            Assert.AreEqual ("Welcome to ASP.NET MVC on Mono!", text);
+		}
+	}
+}
+
